e1a07b7bc83774b91ac6713c6f1d4989b1e51657
Your Name <mohammed.coin.et@gamil.com>
2025-05-08 13:53:04 +0300

    add Header tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import Sidebar from "@/components/Sidebar";
 import { useEffect, useState } from "react";
 import Login from "./page"; // Ensure this is your login page component
 import { useRouter, usePathname } from "next/navigation";
+import { Metadata } from "next"; // Import Metadata type
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,6 +18,54 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+// Define your metadata
+export const metadata: Metadata = {
+  title: {
+    default: "Awash Shop - Your Online Marketplace", // Default title
+    template: "Awash Shop | Home", // Title template for individual pages
+  },
+  description:
+    "Discover a wide range of products at Awash Shop, your trusted online marketplace. Browse electronics, fashion, home goods, and more!",
+  keywords: [
+    "Awash Shop",
+    "online shopping",
+    "marketplace",
+    "electronics",
+    "fashion",
+    "home goods",
+    "ferniture",
+    "clothing",
+  ], // Add relevant keywords
+  authors: [{ name: "Mohammed" }], // Optional: Add author information
+  openGraph: {
+    title: "Awash Shop - Your Online Marketplace",
+    description:
+      "Discover a wide range of products at Awash Shop, your trusted online marketplace. Browse electronics, fashion, home goods, and more!",
+    url: "Awash-dev.com", // Replace with your actual website URL
+    siteName: "Awash Shop",
+    images: [
+      {
+        url: "/icons.jpg", // Replace with the path to your logo image
+        width: 800,
+        height: 600,
+        alt: "Awash Shop Logo",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+  },
+  // Add the favicon link here
+  icons: {
+    icon: "/icons.jpg", // Assuming your favicon is in the public directory
+    apple: "/icons.jpg", // Optional: Apple touch icon
+  },
+};
 
 export default function RootLayout({
   children,
